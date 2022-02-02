3a74526d4f2fd6f7eeb48cd548ab663a1719bdb9
filipemaced <filipe.macedo@decathlon.com>
2022-02-02 17:31:56 +0000

    second commit

---
+++ src/Slider/Slider.svelte
@@ -0,0 +1,42 @@
+<script>
+  // import Swiper core and required modules
+  import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+  import { Swiper, SwiperSlide } from "swiper/svelte";
+
+  // Import Swiper styles
+  import "swiper/css";
+  import "swiper/css/navigation";
+  import "swiper/css/pagination";
+  import "swiper/css/scrollbar";
+
+  let images = [
+    {
+      src: "https://cdn.decathlon.pt/lp/2022/economiacircular/img/main-header.jpg",
+      alt: "container-fitness"
+    },
+    {
+      src: "https://cdn.decathlon.pt/lp/2022/economiacircular/img/main-header.jpg",
+      alt: "container-fitness"
+    },
+    {
+      src: "https://cdn.decathlon.pt/lp/2022/economiacircular/img/main-header.jpg",
+      alt: "container-fitness"
+    }
+  ];
+</script>
+
+<Swiper
+  modules={[Navigation, Pagination, Scrollbar, A11y]}
+  spaceBetween={50}
+  slidesPerView={1}
+  navigation
+  pagination={{ clickable: true }}
+  scrollbar={{ draggable: true }}
+  on:slideChange={() => console.log("slide change")}
+  on:swiper={(e) => console.log(e.detail[0])}
+>
+  {#each images as image, i}
+    <SwiperSlide><img src={image.src} alt="{image.alt}"></SwiperSlide>
+  {/each}
+</Swiper>
